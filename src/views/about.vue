53f850e569b9041b310f0121ace53c4ac46a2e11
liao文禧 <1219585136@qq.com>
2024-05-23 14:15:58 +0800

    init

---
+++ src/views/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+        <button @click="$router.push('/')">To Index Page</button>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { onActivated, onMounted } from 'vue';
+
+defineOptions({
+  name: 'About'
+})
+
+onMounted(() => {
+    console.log('[Index] onMounted')
+})
+
+onActivated(() => {
+    console.log('[Index] onActivated')
+})
+</script>
---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Index Page</h1>
+    <button @click="$router.push('/about')">To About Page</button>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { onActivated, onMounted } from 'vue';
+
+defineOptions({
+  name: 'Index'
+})
+
+onMounted(() => {
+  console.log('[About] onMounted')
+})
+
+onActivated(() => {
+  console.log('[About] onActivated')
+})
+</script>
