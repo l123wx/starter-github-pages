137b78723768918c99a3b400926728f0960c920c
elvis liao <1219585136@qq.com>
2024-11-07 14:44:29 +0800

    feat: automatically registered route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+const pages = import.meta.glob('@/views/**/*.vue')
+
 export default createRouter({
     routes: [
+        ...Object.keys(pages).map(key => {
+            const pageName = key.match(/\/views\/(.+)\.vue$/)?.[1] || ''
+            return {
+                path: `/${pageName.toLowerCase()}`,
+                component: pages[key]
+            }
+        }),
         {
-            path: '/',
-            name: 'Home',
-            component: () => import('../views/index.vue'),
-            children: []
-        }
+            path: '/:pathMatch(.*)*',
+            component: () => import('@/views/index.vue')
+        },
     ],
     history: createWebHashHistory()
 })
